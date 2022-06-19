adea491c62fb952f30dda9e986106329451f8775
Ben Ransford <ben@ransford.org>
2022-06-19 12:51:39 -0700

    get policy by version

--- README.md
+++ README.md
@@ -4,18 +4,22 @@ Versioned policy browser for AWS IAM policies.
 
 # Usage
 
+Show all policies:
+
+    iwas policies
+    iwas list
+
 Show the current version of a policy:
 
     iwas policy <policy ARN>
+    iwas get <policy ARN>
 
 Show the history of a policy:
 
     iwas history <policy ARN>
+    iwas log <policy ARN>
 
 Show a previous version of a policy:
 
     iwas policy <policy ARN> <version>
-
-Show all policies:
-
-    iwas policies
+    iwas get <policy ARN> <version>
--- cmd/policy.go
+++ cmd/policy.go
@@ -2,53 +2,100 @@ package cmd
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/url"
+	"regexp"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/arn"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/iam"
 	"github.com/spf13/cobra"
 )
 
+var iamClient *iam.Client
+
 func init() {
 	rootCmd.AddCommand(policyCmd)
+
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	iamClient = iam.NewFromConfig(cfg)
+}
+
+func getPolicyVersion(arn, version string) (string, error) {
+	if version == "" {
+		getPolicyInput := iam.GetPolicyInput{
+			PolicyArn: &arn,
+		}
+		pol, err := iamClient.GetPolicy(context.Background(), &getPolicyInput)
+		if err != nil {
+			return "", err
+		}
+		version = *pol.Policy.DefaultVersionId
+	}
+
+	getPolicyVersionInput := iam.GetPolicyVersionInput{
+		PolicyArn: &arn,
+		VersionId: &version,
+	}
+	cur, err := iamClient.GetPolicyVersion(context.Background(), &getPolicyVersionInput)
+	if err != nil {
+		return "", err
+	}
+	doc, err := url.QueryUnescape(aws.ToString(cur.PolicyVersion.Document))
+	if err != nil {
+		return "", err
+	}
+	fmt.Printf("// %s\n", version)
+	return doc, nil
+}
+
+func validVersion(v string) bool {
+	_, err := regexp.MatchString(`v[0-9]+`, v)
+	return err == nil
 }
 
 var policyCmd = &cobra.Command{
-	Use:   "policy [arn]",
-	Short: "Show an IAM policy by ARN",
-	Args:  cobra.ExactArgs(1),
+	Use:     "policy <arn> [version]",
+	Aliases: []string{"get"},
+	Short:   "Show an IAM policy by ARN",
+	Args: func(cmd *cobra.Command, args []string) error {
+		// ARN only
+		if len(args) == 1 {
+			if _, err := arn.Parse(args[0]); err != nil {
+				return err
+			}
+			return nil
+		}
+
+		// ARN and version
+		if len(args) == 2 {
+			if _, err := arn.Parse(args[0]); err != nil {
+				return err
+			}
+			if !validVersion(args[1]) {
+				return errors.New("invalid version string")
+			}
+			return nil
+		}
+
+		return errors.New("wrong number of arguments")
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		policyArn := args[0]
-
-		cfg, err := config.LoadDefaultConfig(context.TODO())
+		arn := args[0]
+		version := ""
+		if len(args) == 2 {
+			version = args[1]
+		}
+		pol, err := getPolicyVersion(arn, version)
 		if err != nil {
 			return err
 		}
-		client := iam.NewFromConfig(cfg)
-		getPolicyInput := iam.GetPolicyInput{
-			PolicyArn: &policyArn,
-		}
-		pol, err := client.GetPolicy(context.Background(), &getPolicyInput)
-		if err != nil {
-			return err
-		}
-
-		getPolicyVersionInput := iam.GetPolicyVersionInput{
-			PolicyArn: pol.Policy.Arn,
-			VersionId: pol.Policy.DefaultVersionId,
-		}
-		cur, err := client.GetPolicyVersion(context.Background(), &getPolicyVersionInput)
-		if err != nil {
-			return err
-		}
-		doc, err := url.QueryUnescape(aws.ToString(cur.PolicyVersion.Document))
-		if err != nil {
-			return err
-		}
-
-		fmt.Println(doc)
+		fmt.Println(pol)
 		return nil
 	},
 }
